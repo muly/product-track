b8042071398a3a1d5ba8db327d2dc258a1f48b84
Rohithknaidu <rohith.knaidu0125@gmail.com>
2023-04-05 23:51:07 +0530

    main file is added

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # product-track
 online product price and availability tracking
 
-hi
-hello
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+type product struct{
+	url,price,tracking string
+}
+func main(){
+	
+
+	c:=colly.NewCollector()
+	c.OnHTML("li.product",func(h *colly.HTMLElement) {
+		product:=product{}
+		product.url =h.ChildAttr("a","href")
+		product.price=h.ChildText(".price")
+
+		
+		fmt.Println(product )
+
+	})
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Printf(fmt.Sprintf("visiting %s",r.URL))
+	})
+	c.OnError(func(r *colly.Response, err error) {
+      fmt.Printf("error occured  %s\n",err.Error())
+	})
+	c.Visit("https://scrapeme.live/shop/")
+
+}
