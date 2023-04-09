b2ef81c5da95206870dba3d17a08e5b545a69bad
Rohithknaidu <rohith.knaidu0125@gmail.com>
2023-04-09 20:00:04 +0530

    file is modified

--- main.go
+++ main.go
@@ -6,24 +6,28 @@ import (
 	"github.com/gocolly/colly"
 )
 type product struct{
-	url,price,availabitlity string
+	price,availabitlity string
 }
 func main(){
-    
+    var p product
 	c:=colly.NewCollector()
-	c.OnHTML("li.product",func(h *colly.HTMLElement) {
-		product:=product{}
-		product.url=h.ChildAttr("a","href")
-		product.price=h.ChildText(".price")
-		 fmt.Println(product)
+	c.OnHTML("p.stock.in-stock",func(h *colly.HTMLElement) {
+		p.availabitlity=h.Text
 	
+
 	})
+	c.OnHTML("p.price",func(h *colly.HTMLElement) {
+	  p.price=h.Text
+	  
+	})
+
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Printf(fmt.Sprintf("visiting %s",r.URL))
 	})
 	c.OnError(func(r *colly.Response, err error) {
       fmt.Printf("error occured  %s\n",err.Error())
 	})
-	c.Visit("https://scrapeme.live/shop/")
+	c.Visit("https://scrapeme.live/shop/Charmander/")
+	fmt.Println(p)
 
 }
