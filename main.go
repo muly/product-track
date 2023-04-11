c6362f37e0517d9fd9e49648ad60a1b4110fd5e0
Rohithknaidu <rohith.knaidu0125@gmail.com>
2023-04-11 18:36:14 +0530

    remaining 3 websites are added

--- main.go
+++ main.go
@@ -2,32 +2,113 @@ package main
 
 import (
 	"fmt"
+	"net/url"
+
 
 	"github.com/gocolly/colly"
 )
-type product struct{
-	price,availabitlity string
+
+type product struct {
+	price, availabitlity string
 }
-func main(){
-    var p product
-	c:=colly.NewCollector()
-	c.OnHTML("p.stock.in-stock",func(h *colly.HTMLElement) {
-		p.availabitlity=h.Text
+
+func main() {
+
+	rawURL := "https://www.amazon.com/Sphero-RVR-Programmable-Programmers-Customizable/dp/B0BLF8CLQF"
+
+	 
 	
+	u,err := url.Parse(rawURL)
+    
 
-	})
-	c.OnHTML("p.price",func(h *colly.HTMLElement) {
-	  p.price=h.Text
-	  
-	})
+	if err != nil{
+		// TODO:
+	}
 
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Printf(fmt.Sprintf("visiting %s",r.URL))
-	})
-	c.OnError(func(r *colly.Response, err error) {
-      fmt.Printf("error occured  %s\n",err.Error())
-	})
-	c.Visit("https://scrapeme.live/shop/Charmander/")
-	fmt.Println(p)
+	s := product{}
 
+	switch u.Hostname() {
+	/*case "scrapeme.live":
+		p, err = scrapeme(rawURL)*/
+	/*case "ediblelandscaping.com":
+		p,err=ediblelandscaping(rawURL)*/
+	case "www.amazon.com":
+        s, err=amazon(rawURL)
+	default:
+		fmt.Printf("%s is not supported\n", u.Hostname())
+		return
+
+	}
+
+	if err != nil {
+		// TODO:
+	}
+
+	fmt.Printf("%+v", s)
 }
+
+/*func scrapeme(url string) (product, error) {
+	var p product
+	var err error
+
+	c := colly.NewCollector()
+	c.OnHTML("p.stock.in-stock", func(h *colly.HTMLElement) {
+		p.availabitlity = h.Text
+	})
+	c.OnHTML("p.price", func(h *colly.HTMLElement) {
+		p.price = h.Text
+	})
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Printf(fmt.Sprintf("visiting %s\n", r.URL))
+	})
+	c.OnError(func(r *colly.Response, scrapeErr error) {
+		err = scrapeErr
+	})
+	c.Visit(url)
+
+	return p, err
+}
+func ediblelandscaping(url string) (product, error) {
+	var r product
+	var err error
+
+	c := colly.NewCollector()
+	c.OnHTML("p.note", func(h *colly.HTMLElement) {
+		r.availabitlity = h.Text
+	})
+	c.OnHTML("table.prices", func(h *colly.HTMLElement) {
+	    r.price=h.ChildText("td")
+	})
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Printf(fmt.Sprintf("visiting %s\n", r.URL))
+	})
+	c.OnError(func(r *colly.Response, scrapeErr error) {
+		err = scrapeErr
+	})
+	c.Visit(url)
+
+	return r, err
+}*/
+
+func amazon(url string) (product, error) {
+	var s product
+	var err error
+
+	c := colly.NewCollector()
+	c.OnHTML("", func(h *colly.HTMLElement) {
+		s.availabitlity = h.Text
+	})
+	c.OnHTML("span.a-offscreen", func(h *colly.HTMLElement) {
+	    s.price=h.ChildText(".a-price-whole")
+	})
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Printf(fmt.Sprintf("visiting %s\n", r.URL))
+	})
+	c.OnError(func(r *colly.Response, scrapeErr error) {
+		err = scrapeErr
+	})
+	c.Visit(url)
+
+	return s, err
+}
+
