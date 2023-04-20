cbab474a09da2c4f9db861500f71b1dfcab49ebb
Rohithknaidu <rohith.knaidu0125@gmail.com>
2023-04-20 18:53:23 +0530

    conversion is done in main function

--- main.go
+++ main.go
@@ -3,42 +3,55 @@ package main
 import (
 	"fmt"
 	"net/url"
-
-
 	"github.com/gocolly/colly"
+	"strconv"
 )
 
 type product struct {
-	price, availability string
+	price string
+	 availability string
+}
+type input struct{
+	url string
+	typeOfRequest string 
+	minThreshold float32
 }
 
 func main() {
 
 	rawURL := "https://www.amazon.in/Noise-Launched-Bluetooth-Calling-Tracking/dp/B0BJ72WZQ7/ref=sr_1_2?pd_rd_r=b244f48d-24db-4f55-8fe7-20bbe6ade0d2&pd_rd_w=Iq0zo&pd_rd_wg=EZ5WS&pf_rd_p=3a59b28c-4626-48f9-b66f-114571ee563d&pf_rd_r=XA5YK79N3KZGRAMATR6X&qid=1681233130&sr=8-2&th=1"
-	u,err := url.Parse(rawURL)
-    if err != nil{
-		fmt.Println("error occurred ",err)
+	u, err := url.Parse(rawURL)
+	if err != nil {
+		fmt.Println("error occurred ", err)
 	}
 	p := product{}
-    switch u.Hostname() {
+	price:= p.price 
+	pr,err:= strconv.Atoi(price)
+	if err!=nil{
+		fmt.Println("error")
+	}
+	fmt.Println(pr)
+
+	switch u.Hostname() {
 	case "scrapeme.live":
-		p , err = scrapeme(rawURL)
+		p, err = scrapeme(rawURL)
 	case "ediblelandscaping.com":
-		p ,err=ediblelandscaping(rawURL)
+		p, err = ediblelandscaping(rawURL)
 	case "www.amazon.in":
-        p , err=amazon(rawURL)
+		p, err = amazon(rawURL)
 	default:
 		fmt.Printf("%s is not supported\n", u.Hostname())
 		return
-    }
-    if err != nil {
-		fmt.Println("error occurred",err)
+	}
+	if err != nil {
+		fmt.Println("error occurred", err)
 	}
 
 	fmt.Printf("%+v", p)
 }
 
-func scrapeme(url string) (product, error) {
+
+func scrapeme(url string)(product, error){
 	var p product
 	var err error
 
@@ -48,6 +61,7 @@ func scrapeme(url string) (product, error) {
 	})
 	c.OnHTML("p.price", func(h *colly.HTMLElement) {
 		p.price = h.Text
+
 	})
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Printf(fmt.Sprintf("visiting %s\n", r.URL))
@@ -56,8 +70,7 @@ func scrapeme(url string) (product, error) {
 		err = scrapeErr
 	})
 	c.Visit(url)
-
-	return p, err
+   return p, err
 }
 func ediblelandscaping(url string) (product, error) {
 	var p product
@@ -68,7 +81,7 @@ func ediblelandscaping(url string) (product, error) {
 		p.availability = h.Text
 	})
 	c.OnHTML("table.prices", func(h *colly.HTMLElement) {
-	    p.price=h.ChildText("td")
+		p.price = h.ChildText("td")
 	})
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Printf(fmt.Sprintf("visiting %s\n", r.URL))
@@ -77,6 +90,7 @@ func ediblelandscaping(url string) (product, error) {
 		err = scrapeErr
 	})
 	c.Visit(url)
+	
 
 	return p, err
 }
@@ -89,9 +103,10 @@ func amazon(url string) (product, error) {
 	c.OnHTML("#availability", func(h *colly.HTMLElement) {
 		p.availability = h.Text
 	})
-	c.OnHTML("div.a-section.a-spacing-none.aok-align-center",func(h *colly.HTMLElement) {
-		p.price=h.ChildText("span.a-price-whole")
+	c.OnHTML("div.a-section.a-spacing-none.aok-align-center", func(h *colly.HTMLElement) {
+		p.price = h.ChildText("span.a-price-whole")
 	})
+
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Printf(fmt.Sprintf("visiting %s\n", r.URL))
@@ -103,4 +118,13 @@ func amazon(url string) (product, error) {
 
 	return p, err
 }
+func notify(i input)(bool,error){
+	var err error
+	//var p product
+	if err!=nil{
+		fmt.Println(err)
+    }
 
+
+	return true,err
+}
