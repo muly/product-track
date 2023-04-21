6606c07b8fc20666b2bb735a4b91ea94075f2a44
Rohithknaidu <rohith.knaidu0125@gmail.com>
2023-04-21 22:37:41 +0530

    all changes are made

--- main.go
+++ main.go
@@ -10,13 +10,13 @@ import (
 )
 
 type product struct {
-	price int
-	 availability bool
+	price        float64
+	availability bool
 }
-type input struct{
-	url string
-	typeOfRequest string 
-	minThreshold float32
+type input struct {
+	url           string
+	typeOfRequest string
+	minThreshold  float32
 }
 
 func main() {
@@ -42,35 +42,32 @@ func main() {
 	if err != nil {
 		fmt.Println("error occurred", err)
 	}
-	
-   fmt.Printf("%+v",p)
 
-	
+	fmt.Printf("%+v", p)
+
 }
 
-
-func scrapeme(url string)(product, error){
+func scrapeme(url string) (product, error) {
 	var p product
 	var err error
 
 	c := colly.NewCollector()
 	c.OnHTML("p.stock.in-stock", func(h *colly.HTMLElement) {
-		availability:= h.Text  
-		if availability=="In stock"{
-            p.availability=true 
-		}else{
-			p.availability=false
+		availability := h.Text
+		if availability == "In stock" {
+			p.availability = true
+		} else {
+			p.availability = false
 		}
-
 
 	})
 	c.OnHTML("p.price", func(h *colly.HTMLElement) {
-		price:= h.Text
-		s,err:=strconv.Atoi(price)
-		if err != nil{
+		price := h.Text
+		s, err := strconv.ParseFloat(price,10)
+		if err != nil {
 			log.Printf("error")
 		}
-		p.price=s
+		p.price = s
 
 	})
 	c.OnRequest(func(r *colly.Request) {
@@ -80,7 +77,7 @@ func scrapeme(url string)(product, error){
 		err = scrapeErr
 	})
 	c.Visit(url)
-   return p, err
+	return p, err
 }
 func flipkart(url string) (product, error) {
 	var p product
@@ -88,20 +85,20 @@ func flipkart(url string) (product, error) {
 
 	c := colly.NewCollector()
 	c.OnHTML("div._2JC05C", func(h *colly.HTMLElement) {
-		availability:= h.Text
-		if availability==" In stock "{
-			p.availability=true
-		}else{
-		    p.availability=false
+		availability := h.Text
+		if availability == " In stock " {
+			p.availability = true
+		} else {
+			p.availability = false
 		}
 	})
-	c.OnHTML("div._30jeq3._16Jk6d",func(h *colly.HTMLElement) {
-		price:=h.Text
-		s,err:=strconv.Atoi(price)
-		if err!=nil{
+	c.OnHTML("div._30jeq3._16Jk6d", func(h *colly.HTMLElement) {
+		price := h.Text
+		s, err := strconv.ParseFloat(price,10)
+		if err != nil {
 			log.Printf("error")
 		}
-		p.price=s
+		p.price = s
 	})
 
 	c.OnRequest(func(r *colly.Request) {
@@ -111,7 +108,6 @@ func flipkart(url string) (product, error) {
 		err = scrapeErr
 	})
 	c.Visit(url)
-	
 
 	return p, err
 }
@@ -119,27 +115,45 @@ func flipkart(url string) (product, error) {
 func amazon(url string) (product, error) {
 	var p product
 	var err error
-
+    var statement string
 	c := colly.NewCollector()
 	c.OnHTML("#availability", func(h *colly.HTMLElement) {
-		availability:= h.Text 
-		if availability==" In stock "{
-			p.availability=true
-		}else{
-			p.availability=false 
+		statement = h.Text
+		fmt.Println(statement)
+		if statement == "                                                In stock                               " {
+			p.availability = true
+		} else {
+			p.availability = false
 		}
-		
+
 	})
 	c.OnHTML("div.a-section.a-spacing-none.aok-align-center", func(h *colly.HTMLElement) {
-		price:= h.ChildText("span.a-price-whole")
-		r,err:= strconv.Atoi(price)
-		if err != nil {
-			fmt.Println("error")
+		price := h.ChildText("span.a-price-whole")
+        var f float64
+		var j float64
+		j=1
+		var i int
+		for i=0 ;i<len(price);i++ {
+			if price[i]>='0' && price[i]<='9'{
+				//fmt.Printf("%T",price[i]-48)
+                f=(f*10 ) + float64(price[i]-48)	
+			}
+			if price[i]=='.'{
+               break
+			}
+		}
+		for i<len(price){
+			if price[i]>='0' && price[i]<='9'{
+			  j=j*10
+              f=f+float64(price[i]-48)/j
+
+			}
+		 i++	
 		}
 
-		p.price=r
-	})
 
+		p.price = f
+	})
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Printf(fmt.Sprintf("visiting %s\n", r.URL))
@@ -151,13 +165,13 @@ func amazon(url string) (product, error) {
 
 	return p, err
 }
+
 //func shouldNotify(i input)(bool,error){
-	//var err error
-	//var p product
-	//if err!=nil{
-		//fmt.Println(err)
-    //}
+//var err error
+//var p product
+//if err!=nil{
+//fmt.Println(err)
+//}
 
-
-	//return true,err
+//return true,err
 //}
