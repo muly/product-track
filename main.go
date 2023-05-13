b452c86095b2f50cbd7b916e66671b1835f73993
muly <msrinivasareddy@gmail.com>
2023-05-13 09:24:43 -0400

    refactor main

--- main.go
+++ main.go
@@ -8,28 +8,31 @@ import (
 
 func main() {
 	rawURL := "https://www.flipkart.com/hp-deskjet-1112-single-function-color-inkjet-printer/p/itme9wgtatxfzggg?pid=PRNE9WGTZCQGJ6PZ&lid=LSTPRNE9WGTZCQGJ6PZND3GV1&marketplace=FLIPKART&store=6bo%2Ftia%2Fffn%2Ft64&srno=b_3_98&otracker=hp_omu_Best%2Bof%2BElectronics_2_3.dealCard.OMU_D54DFY00C5JD_3&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_2_L2_view-all%2Chp_omu_PINNED_neo%2Fmerchandising_Best%2Bof%2BElectronics_NA_dealCard_cc_2_NA_view-all_3&fm=neo%2Fmerchandising"
-	u, err := url.Parse(rawURL)
-	if err != nil {
-		fmt.Println("invalid url ", err)
-		return
-	}
-	p := product{}
 
-	switch u.Hostname() {
-	case "scrapeme.live":
-		p, err = scrapeme(rawURL)
-	case "www.flipkart.com" :
-		p, err = flipkart(rawURL)
-	case "www.amazon.in":
-		p, err = amazon(rawURL)
-	default:
-		fmt.Printf("%s is not supported\n", u.Hostname())
-		return
-	}
+	p, err := process(rawURL)
 	if err != nil {
 		fmt.Println("error occurred while scraping", err)
 		return
 	}
 
 	fmt.Printf("%+v", p)
+}
+
+func process(rawURL string) (product, error) {
+	u, err := url.Parse(rawURL)
+	if err != nil {
+		return product{}, err
+	}
+
+	switch u.Hostname() {
+	case "scrapeme.live":
+		return scrapeme(rawURL)
+	case "www.flipkart.com":
+		return flipkart(rawURL)
+	case "www.amazon.in":
+		return amazon(rawURL)
+	default:
+		fmt.Printf("%s is not supported\n", u.Hostname())
+		return product{}, err
+	}
 }
--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -154,6 +155,32 @@ func Test_checkAvailability(t *testing.T) {
 			if got := checkAvailability(tt.args.s); got != tt.want {
 				t.Errorf("checkAvailability() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func Test_process(t *testing.T) {
+
+	tests := []struct {
+		name    string
+		rawURL  string
+		wantErr bool
+	}{
+		{
+			name:    "flipcart",
+			rawURL:  "https://www.flipkart.com/hp-deskjet-1112-single-function-color-inkjet-printer/p/itme9wgtatxfzggg?pid=PRNE9WGTZCQGJ6PZ&lid=LSTPRNE9WGTZCQGJ6PZND3GV1&marketplace=FLIPKART&store=6bo%2Ftia%2Fffn%2Ft64&srno=b_3_98&otracker=hp_omu_Best%2Bof%2BElectronics_2_3.dealCard.OMU_D54DFY00C5JD_3&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_2_L2_view-all%2Chp_omu_PINNED_neo%2Fmerchandising_Best%2Bof%2BElectronics_NA_dealCard_cc_2_NA_view-all_3&fm=neo%2Fmerchandising",
+			wantErr: false,
+		},
+		// TODO: add a url for each websites we support
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := process(tt.rawURL)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("process() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			fmt.Println(got)
 		})
 	}
 }
