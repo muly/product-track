40d6c54e1a2b99909d4687153c5e479f089fcf34
Rohithknaidu <rohith.knaidu0125@gmail.com>
2023-06-10 00:20:18 +0530

    one more testcase is added in main_test.go

--- main_test.go
+++ main_test.go
@@ -183,12 +183,12 @@ func Test_process(t *testing.T) {
 			wantErr: false,
 
 		},
-		//{
-			// name: "flipkart",
-			// rawURL: "https://www.flipkart.com/hp-deskjet-1112-single-function-colorprinter/p/itme9wgtatxfzggg?pid=PRNE9WGTZCQGJ6PZ&lid=LSTPRNE9WGTZCQGJ6PZND3GV1&marketplace=FLIPKART&store=6bo%2Ftia%2Fffn%2Ft64&srno=b_3_98&otracker=hp_omu_Best%2Bof%2BElectronics_2_3.dealCard.OMU_D54DFY00C5JD_3&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_2_L2_view-all%2Chp_omu_PINNED_neo%2Fmerchandising_Best%2Bof%2BElectronics_NA_dealCard_cc_2_NA_view-all_3&fm=neo%2Fmerchandising",
-			// wantErr: true,
+		{
+			 name: "flipkart",
+			 rawURL: "https://www.flipkart.com/hp-deskjet-1112-single-function-colorprinter/p/itme9wgtatxfzggg?pid=PRNE9WGTZCQGJ6PZ&lid=LSTPRNE9WGTZCQGJ6PZND3GV1&marketplace=FLIPKART&store=6bo%2Ftia%2Fffn%2Ft64&srno=b_3_98&otracker=hp_omu_Best%2Bof%2BElectronics_2_3.dealCard.OMU_D54DFY00C5JD_3&otracker1=hp_rich_navigation_PINNED_neo%2Fmerchandising_NA_NAV_EXPANDABLE_navigationCard_cc_2_L2_view-all%2Chp_omu_PINNED_neo%2Fmerchandising_Best%2Bof%2BElectronics_NA_dealCard_cc_2_NA_view-all_3&fm=neo%2Fmerchandising",
+			 wantErr: true,
 
-		//},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
