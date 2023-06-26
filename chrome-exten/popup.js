31ebd275d486c60f51e5edc3203c0671f5af3667
Rohithknaidu <rohith.knaidu0125@gmail.com>
2023-06-27 00:02:57 +0530

    pricethreshold field is changed for testing purpose

--- chrome-exten/popup.js
+++ chrome-exten/popup.js
@@ -54,7 +54,7 @@ document.addEventListener('DOMContentLoaded', function() {
             headers : { "Content-Type" : "application/json" } ,
             body: JSON.stringify({
                 url:"https://www.flipkart.com/samsung-galaxy-f13-nightsky-green-64-gb/p/itmeadfda1bd23fa?pid=MOBGENJWF4KJTPEN&lid=LSTMOBGENJWF4KJTPENS2XJXA&marketplace=FLIPKART&store=tyy%2F4io&srno=b_1_1&otracker=clp_banner_1_14.bannerX3.BANNER_mobile-phones-store_ARV72AV1ALWY&fm=neo%2Fmerchandising&iid=d2edff5a-41be-41c5-825a-89fbf189399b.MOBGENJWF4KJTPEN.SEARCH&ppt=clp&ppn=mobile-phones-store&ssid=m55kbf5z280000001686801244020",
-                min_Threshold: 1500
+                min_Threshold: minPrice
             })
         })
             .then((req) => {
