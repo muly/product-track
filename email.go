a98524e49947c933601d22df85d902f039e6dd9f
Rohithknaidu <rohith.knaidu0125@gmail.com>
2023-07-08 17:14:01 +0530

    trying to fix logs in email function'

--- email.go
+++ email.go
@@ -12,7 +12,7 @@ var emailClient *mail.Dialer
 //function for initializing email 
 func initEmailClient() {
 	password := os.Getenv("GMAIL_PASSWORD")
-	emailClient = mail.NewDialer("smtp.gmail.com", 587, "rohith.knaidu0125@gmail.com", password)
+	emailClient = mail.NewDialer("smtp.gmail.com", 587, "smulytestground@gmail.com", password)
 }
 
 //function for sending email to the user according to the type of request
@@ -20,8 +20,8 @@ func sendEmail()(*mail.Message) {
 	var t trackInput
 	log.Println("creating mail")
 	m := mail.NewMessage()
-	m.SetHeader("From", "rohith.knaidu0125@gmail.com")
-	m.SetHeader("To", "smulytestground@gmail.com")
+	m.SetHeader("From", "smulytestground@gmail.com")
+	m.SetHeader("To", "rohith.knaidu0125@gmail.com")
 	if err := emailClient.DialAndSend(m); err != nil {
 		log.Println("Error sending email:", err)
 		return nil
