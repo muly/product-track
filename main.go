00b79b0a6fde346e6e741410a190fba157e5b922
Rohithknaidu <rohith.knaidu0125@gmail.com>
2023-07-08 23:58:37 +0530

    secret manager is created in main.go file

--- main.go
+++ main.go
@@ -2,17 +2,70 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/url"
+
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
 )
+
+var secretManagerClient *secretmanager.Client
 
 func main() {
 	initFirestore(context.Background())
 	initEmailClient()
 	handleRequest()
+	projectID := "smuly-test-ground"
+
+	// Create the client.
+	ctx := context.Background()
+	var err error
+	secretManagerClient, err = secretmanager.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("failed to setup client: %v", err)
+	}
+	defer secretManagerClient.Close()
+
+	// Create the request to create the secret.
+	createSecretReq := &secretmanagerpb.CreateSecretRequest{
+		Parent:   fmt.Sprintf("projects/%s", projectID),
+		SecretId: "test123",
+		Secret: &secretmanagerpb.Secret{
+			Replication: &secretmanagerpb.Replication{
+				Replication: &secretmanagerpb.Replication_Automatic_{
+					Automatic: &secretmanagerpb.Replication_Automatic{},
+				},
+			},
+		},
+	}
+
+	secret, err := secretManagerClient.CreateSecret(ctx, createSecretReq)
+	if err != nil {
+		log.Fatalf("failed to create secret: %v", err)
+	}
+	payload := []byte("nihavebfxhzbkhml")
+	addSecretVersionReq := &secretmanagerpb.AddSecretVersionRequest{
+		Parent: secret.Name,
+		Payload: &secretmanagerpb.SecretPayload{
+			Data: payload,
+		},
+	}
+	version, err := secretManagerClient.AddSecretVersion(ctx, addSecretVersionReq)
+	if err != nil {
+		log.Fatalf("failed to add secret version: %v", err)
+	}
+	accessRequest := &secretmanagerpb.AccessSecretVersionRequest{
+		Name: version.Name,
+	}
+	result, err := secretManagerClient.AccessSecretVersion(ctx, accessRequest)
+	if err != nil {
+		log.Fatalf("failed to access secret version: %v", err)
+	}
+	log.Printf("Plaintext: %s", result.Payload.Data)
 }
 
-//function for processing a url according the url provided
+// function for processing a url according the url provided
 func process(rawURL string) (product, error) {
 	u, err := url.Parse(rawURL)
 	if err != nil {
