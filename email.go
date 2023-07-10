a8cca28cf200c335e433329ed01fb8276846200b
Rohithknaidu <rohith.knaidu0125@gmail.com>
2023-07-10 18:58:31 +0530

    user is changed

--- email.go
+++ email.go
@@ -11,15 +11,15 @@ var emailClient *mail.Dialer
 // function for initializing email
 func initEmailClient() {
 	password :=string(payloadData)
-	emailClient = mail.NewDialer("smtp.gmail.com", 587, "smulytestground@gmail.com", password)
+	emailClient = mail.NewDialer("smtp.gmail.com", 587, "rohith.knaidu0125@gmail.com", password)
 }
 
 // function for sending email to the user according to the type of request
 func sendEmail(t trackInput) error {
 	log.Println("creating mail")
 	m := mail.NewMessage()
-	m.SetHeader("From", "smulytestground@gmail.com")
-	m.SetHeader("To", "rohith.knaidu0125@gmail.com")
+	m.SetHeader("From", "rohith.knaidu0125d@gmail.com")
+	m.SetHeader("To", "smulytestground@gmail.com")
 	if t.TypeOfRequest == requestTypeAvailability {
 		m.SetHeader("Subject", "Availability update Notification")
 		m.SetBody("text/plain", "product %s is available"+t.Url)
