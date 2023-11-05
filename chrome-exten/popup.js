48db03bf809dd3fb92fe28a7fa317c099bbd2af3
Rohithknaidu <rohith.knaidu0125@gmail.com>
2023-11-05 20:41:25 +0530

    pop up page is changed and in  manifest file client id is changed to local client id

--- chrome-exten/popup.js
+++ chrome-exten/popup.js
@@ -1,4 +1,5 @@
-var apiurl=`https://smuly-test-ground.ue.r.appspot.com`
+//var apiurl=`https://smuly-test-ground.ue.r.appspot.com`
+var apiurl=`http:localhost:8006`
 let emailid 
 chrome.identity.getAuthToken({ interactive: true, scopes: ['email'] }, function(token) {
   if (chrome.runtime.lastError) {
@@ -16,63 +17,89 @@ chrome.identity.getAuthToken({ interactive: true, scopes: ['email'] }, function(
       emailid=data.email
   })
   })
-document.addEventListener('DOMContentLoaded', function() {
+  document.addEventListener('DOMContentLoaded', function() {
+    // Function to fetch active tab URL
     function fetchActiveTabURL(callback) {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        if (tabs && tabs.length > 0) {
-        var url = tabs[0].url;
-        callback(url);
-        }
-      });
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            if (tabs && tabs.length > 0) {
+                var url = tabs[0].url;
+                callback(url);
+            }
+        });
     }
+
+    // DOM elements
+    const availabilityFields = document.getElementById('availabilityFields');
+    const priceFields = document.getElementById('priceFields');
+    const availabilityOption = document.querySelector('input[value="availability"]');
+    const priceOption = document.querySelector('input[value="price"]');
+    const submitBtn = document.getElementById('submitBtn');
+
+    // Fetch active tab URL
     var activeTabURL;
     fetchActiveTabURL(function(url) {
-    activeTabURL = url;
-    var submitBtn = document.getElementById('submitBtn');
-    submitBtn.addEventListener('click', function(event) {  
-      event.preventDefault();
-      var selectedOption = document.querySelector('input[name="option"]:checked').value;
-      var minPriceThreshold = document.getElementById('minPrice').value;
-      if (selectedOption === 'availability') {
-        fetch(apiurl+`/track/availability`, {
-            method: "POST",
-            mode:"no-cors",
-            headers : { "Content-Type" : "application/json" } ,
-            body: JSON.stringify({
-              url:activeTabURL,
-              emailid:emailid
+        activeTabURL = url;
+    });
+
+    // Event listener for radio button changes
+    availabilityOption.addEventListener('change', function() {
+        if (availabilityOption.checked) {
+            availabilityFields.style.display = 'block';
+            priceFields.style.display = 'none';
+        }
+    });
+
+    priceOption.addEventListener('change', function() {
+        if (priceOption.checked) {
+            availabilityFields.style.display = 'none';
+            priceFields.style.display = 'block';
+        }
+    });
+    submitBtn.addEventListener('click', function(event) {
+        event.preventDefault();
+        var selectedOption = document.querySelector('input[name="option"]:checked').value;
+        var minPriceThreshold = document.getElementById('minPrice').value;
+        var zipCode = document.getElementById('zipCode').value;
+            if (selectedOption === 'availability') {
+            fetch(apiurl+'/track/availability', {
+                method: "POST",
+                mode: "no-cors",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    url: activeTabURL,
+                    emailid: emailid,
+                    zipcode: zipCode
+                })
             })
-        })
-            .then((req) => {
-                alert("succesfull") ;
+            .then((response) => {
+                alert("Successful");
                 window.close();
             })
             .catch((err) => {
-                console.log(err. Message)
+                console.log(err.message);
+            });
+        } else if (selectedOption === 'price') {
+            fetch(apiurl+'/track/price', {
+                method: "POST",
+                mode: "no-cors",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    url: activeTabURL,
+                    min_threshold: parseFloat(minPriceThreshold),
+                    emailid: emailid
+                })
             })
-      }
-      else if (selectedOption === 'price') {
-          fetch(apiurl+`/track/price`, {
-              method: "POST",
-              mode:"no-cors",
-              headers : { "Content-Type" : "application/json" } ,
-              body: JSON.stringify({
-                  url:activeTabURL,
-                  min_threshold:parseFloat(minPriceThreshold),
-                  emailid:emailid
-              })
-          })
-              .then((req) => {
-                  alert('Successful') ;
-                  window.close();
-              })
-              .catch((err) => {
-                console.log(err. Message)
-              })
-        }  
+            .then((response) => {
+                alert("Successful");
+                window.close();
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+        }
     });
-  });
 });
+
       
     
     
