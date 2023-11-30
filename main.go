972e78f77a14e69eb533a423c97f03bb44b671d8
Rohithknaidu <rohith.knaidu0125@gmail.com>
2023-11-30 23:48:38 +0530

    privacy policy html file is deployed in local

--- main.go
+++ main.go
@@ -45,6 +45,9 @@ func main() {
 		http.ServeFile(w, r, "./integration_testing/mock_websites/"+path)
 	})
 
+	router.GET("/privacypolicy.html", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		http.ServeFile(w, r, "./chrome-exten/privacyPolicy.html")
+	})
 	// Start the server
 	log.Fatal(http.ListenAndServe(":"+port, router))
 
